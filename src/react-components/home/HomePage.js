1e9ca708fd7b9263b95d97f9b4fda18dd66243c5
Markus Traber <hi@markustraber.com>
2022-10-17 13:50:06 +0200

    Disable Connection Test for now

--- src/react-components/home/HomePage.js
+++ src/react-components/home/HomePage.js
@@ -188,9 +188,9 @@ export function HomePage() {
           </Column>
         </Container>
       )}
-      <Container>
+      {/* <Container>
         <ConnectionTest />
-      </Container>
+      </Container> */}
       {isHmc() ? (
         <Column center>
           <SocialBar />
